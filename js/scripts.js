fbc07508520b0e73ab625e84c25067068896558b
Jaime Cabrera <84362202+jaimecabrea@users.noreply.github.com>
2025-12-26 20:46:35 -0800

    editing porfolio about

--- js/scripts.js
+++ js/scripts.js
@@ -39,11 +39,7 @@ var typewriter = new Typewriter(app, {
     deleteSpeed:5
 });
 
-typewriter.typeString('software engineer.')
-    .pauseFor(1000)
-    .deleteAll()
-    .pauseFor(500)
-    .typeString("creative thinker.")
+typewriter.typeString("creative thinker.")
     .pauseFor(1000)
     .deleteAll()
     .pauseFor(500)
